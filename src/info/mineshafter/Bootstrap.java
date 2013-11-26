2d00f656832117a9cb92110c4c44575cd89cf3fa
Erin <download333@gmail.com>
2013-11-26 00:43:41 -0800

    Version number increment

--- src/info/mineshafter/Bootstrap.java
+++ src/info/mineshafter/Bootstrap.java
@@ -36,7 +36,7 @@ public class Bootstrap extends JFrame {
 
 	private static final long serialVersionUID = 1;
 	private static int bootstrapVersion = 4;
-	private static int mineshafterBootstrapVersion = 5;
+	private static int mineshafterBootstrapVersion = 6;
 
 	private final File workDir;
 	private final File launcherJar;
