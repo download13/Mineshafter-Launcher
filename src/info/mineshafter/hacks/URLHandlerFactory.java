b09ee291bf52121ffec9171f36ad8f780f1308d0
DarkLBP <darklbp@outlook.com>
2016-08-17 13:44:43 +0200

    Supported textures handling (skins and capes now working)

--- src/info/mineshafter/hacks/URLHandlerFactory.java
+++ src/info/mineshafter/hacks/URLHandlerFactory.java
@@ -6,10 +6,12 @@ import java.net.URLStreamHandlerFactory;
 public class URLHandlerFactory implements URLStreamHandlerFactory {
 	public URLStreamHandler createURLStreamHandler(String protocol) {
 		// If protocol is HTTP or HTTPS check if we want to intercept
-		if (protocol.equalsIgnoreCase("http") || protocol.equalsIgnoreCase("rawhttp")) {
-			// Nothing we need to worry about is going over HTTP
-			return new sun.net.www.protocol.http.Handler();
-		} else if (protocol.equalsIgnoreCase("https")) {
+		if (protocol.equalsIgnoreCase("http")) {
+			return new InterceptHttpHandler();
+		} else if (protocol.equalsIgnoreCase("rawhttp")){
+                    // Nothing we need to worry about is going over HTTP
+                    return new sun.net.www.protocol.http.Handler();
+                } else if (protocol.equalsIgnoreCase("https")) {
 			return new InterceptHttpsHandler();
 		} else if (protocol.equalsIgnoreCase("rawhttps")) {
 			// Just in case we need to call the real destination
